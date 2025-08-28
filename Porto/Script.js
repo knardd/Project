86a6ec090c5a2805741c4ad08fb88b374fc8e081
kennard <kenkum08@gmail.com>
2025-08-28 23:41:19 +0700

    Add files via upload

---
+++ Porto/Script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple.text", {
+  strings: ["Web Development", "Student"],
+  typeSpeed: 120, // kecepatan mengetik
+  backSpeed: 120, // kecepatan menghapus
+  backDelay: 1000,
+  loop: true,
+});
